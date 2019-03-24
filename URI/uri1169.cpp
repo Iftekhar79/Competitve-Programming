f23ff6cfb61edde50112d3f266bca6608ee5699c
SM Iftekhar Alam Tipu <38460302+Iftekhar79@users.noreply.github.com>
2019-03-24 20:01:55 +0600

    Add files via upload

---
+++ URI/uri1169.cpp
@@ -0,0 +1,26 @@
+// Grains in a Chess Board
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	
+	int test,num;
+	unsigned long long int res;
+	
+	cin>>test;
+	
+	while(test--){
+		cin>>num;
+		
+		res=pow(2,num)/12000;
+		
+		cout<<res<<" kg"<<endl;
+		
+		
+	}
+	
+	
+	
+}
