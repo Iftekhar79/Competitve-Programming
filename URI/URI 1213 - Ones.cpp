52ee200dd38b73e5c9699eabe9157655509c984c
SM Iftekhar Alam Tipu <38460302+Iftekhar79@users.noreply.github.com>
2020-02-29 15:56:01 +0600

    Implementation, Beginner

---
+++ URI/URI 1213 - Ones.cpp
@@ -0,0 +1,36 @@
+//Ones
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	
+
+	int n;
+	int cnt;
+	
+		
+	while(cin>>n){
+	
+		cnt=0;
+		long long int sum=0;
+		
+		while(1){
+			
+			sum=(sum*10+1)%n;
+			cnt++;
+			
+			if(sum==0)
+			break;		
+			
+		}
+		
+		
+		cout<<cnt<<"\n";
+		
+	}
+	
+	
+	
+}
